52b59bdf458681d1a942653748d0e4b853cabb86
greensparker <gurleenk4.23@gmail.com>
2018-08-08 16:11:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
 let currentUser = "Gurleen"
-let welcomeMessage = "Welcome to Klossybook, Gurleen!"
+let welcomeMessage = "Welcome to Klossybook, " + currentUser + "!"
 let excitedWelcomeMessage = "WELCOME TO KLOSSYBOOK, GURLEEN!"
 let shortGreeting = "Welcome, G!"
